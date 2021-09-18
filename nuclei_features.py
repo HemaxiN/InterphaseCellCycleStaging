6a800419776ebf119d139f466243cdfb7dfa4e4a
Hemaxi Narotamo <41121368+HemaxiN@users.noreply.github.com>
2021-09-18 09:21:09 +0100

    Update nuclei_features.py

--- nuclei_features.py
+++ nuclei_features.py
@@ -4,40 +4,29 @@ import pickle
 import pandas as pd
 import numpy as np 
 from skimage.segmentation import clear_border
-from skimage.measure import regionprops, shannon_entropy
-import matplotlib.pyplot as plt
-from skimage.feature import local_binary_pattern, greycomatrix
-from skimage import exposure
+from skimage.measure import regionprops, label
 import skimage
-from skimage.measure import label
 
 def obtain_features(images_path, predictions_path):
 	#create the dataframe
 	features = pd.DataFrame(columns = ["Image", "Area", "Total Intensity", "Mean Intensity", 
 									  "Nucleus Patch", "bbox"])
-
 	#for each image and prediction, read the image, load the pickle object
 	for img, pred in zip(os.listdir(images_path), os.listdir(predictions_path)):
 		#read the image
 		image = cv2.imread(os.path.join(images_path, img), cv2.IMREAD_GRAYSCALE)
-
 		#folder containing binary masks 
 		masks = cv2.imread(os.path.join(predictions_path, pred), cv2.IMREAD_GRAYSCALE)
-
 		masks = skimage.morphology.remove_small_objects(masks , min_size = 20)        
 		masks_labelled = lbl2(masks)	        
-
 		#compute features for each nuclei and add to the features dataframe
 		features = compute_props(image, masks_labelled, features, img)
-
 	return features
-
 
 def lbl2(masks):
 	#label the masks and clear the borders 
 	#masks = label(masks) --> the output masks from https://github.com/HemaxiN/YOLO_UNET are already labeled
 	prediction = clear_border(masks, buffer_size=1)
-
 	return prediction
 
 def compute_props(image, masks_labelled, features, img):
@@ -58,30 +47,22 @@ def compute_props(image, masks_labelled, features, img):
 		ymin = bbox_aux[0]
 		xmax = bbox_aux[3]
 		ymax = bbox_aux[2]    
-	
-
-		if(True):     
-			res = {"Image": img, "Area": area, "Total Intensity": total_intensity, "Mean Intensity": mean_intensity,
-				 	"Nucleus Patch": saving_img, "bbox": bbox_aux}
-			row = len(features)
-			features.loc[row] = res
+		
+		res = {"Image": img, "Area": area, "Total Intensity": total_intensity, "Mean Intensity": mean_intensity,
+				"Nucleus Patch": saving_img, "bbox": bbox_aux}
+		row = len(features)
+		features.loc[row] = res
 	return features
 
 def create_new_table(pandasfilepath):
 	df = pd.DataFrame(columns=["Image", "Area", "Total Intensity", "Mean Intensity", "Nucleus Patch", "bbox"])
 	df.to_pickle(pandasfilepath)
 	
-
 def save_pd(pandasfilepath, result):
 	result.to_pickle(pandasfilepath)    
 
 ''' process the dataframe to include the normalized area and normalized total intensity''' 
-
-
 def process_dataframe(df_aux):
-
-	import numpy as np
-
 	var = df_aux.groupby(['Image'], as_index = False).var()
 	mean = df_aux.groupby(['Image'], as_index = False).mean()
 
@@ -90,52 +71,27 @@ def process_dataframe(df_aux):
 
 	for index, row in df_aux.iterrows():
 	    #print(row['Area'], row['Total Intensity'])
-	    
-	    
 	    img_name = row['Image']
-	    
 	    area_mean =  mean[mean['Image'] == img_name]['Area']
-	    
 	    area_mean = area_mean.as_matrix()[0]
-	    
 	    area_var = var[var['Image'] == img_name]['Area']
-	    
 	    area_var = area_var.as_matrix()[0]
-	    
 	    area = row['Area']
-	    
 	    n_area = (area - area_mean) / (np.sqrt(area_var))
-	    
-	    
-	    
 	    intensity_mean =  mean[mean['Image'] == img_name]['Total Intensity']
-	    
 	    intensity_mean = intensity_mean.as_matrix()[0]
-	    
 	    intensity_var = var[var['Image'] == img_name]['Total Intensity']
-	    
 	    intensity_var = intensity_var.as_matrix()[0]
-	    
 	    intensity = row['Total Intensity']    
-	    
 	    n_intensity = (intensity - intensity_mean) / (np.sqrt(intensity_var))
-	    
-	    
 	    normalized_area.append(n_area)
 	    normalized_total_intensity.append(n_intensity)
 	    
-
-	import pandas as pd
-
-
 	normalized_area_df = pd.Series( (v for v in normalized_area) ) 
 	normalized_total_intensity_df = pd.Series((v for v in normalized_total_intensity))
-	    
-	    
-
+	 
 	df_aux['norm_area'] = normalized_area_df
 	df_aux['norm_intensity'] = normalized_total_intensity_df
 	    
-    
 	return df_aux
 
