9c6b06bc48fe0540dd8dbdd0909584cdca15faa7
Hemaxi Narotamo <41121368+HemaxiN@users.noreply.github.com>
2021-09-18 09:41:16 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -15,6 +15,13 @@ save_dir = r'C:\Users\hemaxi\Desktop\cell_cycle\class' ##directory where the ima
 def cell_cycle_classifier(imgs_dir, msks_dir, save_dir):
 	features = obtain_features(imgs_dir, msks_dir)
 	features = process_dataframe(features)
-	classify(features, save_dir, imgs_dir)
+	df, y_pred_vec = classify(features, save_dir, imgs_dir)
+	y_pred_vec = np.asarray(y_pred_vec)[0]
+	df['pred_S_G2'] = y_pred_vec
+	y_pred_vec = 1-y_pred_vec
+	df['pred_G1'] = y_pred_vec
+	
+	final_metrics = df.groupby(["Image"], as_index=False).agg({'pred_G1': np.sum, 'pred_S_G2': np.sum})
+	final_metrics.to_csv(os.path.join(save_dir, 'results.csv'))
 
 cell_cycle_classifier(img_dir, msk_dir, save_dir)
