257c7d406c46222c340ca0d9759a174b6749d4ad
Hemaxi Narotamo <41121368+HemaxiN@users.noreply.github.com>
2021-09-18 09:41:40 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 img_dir = r'C:\Users\hemaxi\Desktop\cell_cycle\imgs' ##directory where the images are saved
 msk_dir = r'C:\Users\hemaxi\Desktop\cell_cycle\label'  ##directory where the labeled masks are saved
-save_dir = r'C:\Users\hemaxi\Desktop\cell_cycle\class' ##directory where the images are saved,
+save_dir = r'C:\Users\hemaxi\Desktop\cell_cycle\class' ##directory where the images will be saved
 ## after performing classification, nuclei classified as S/G2 will have a green bounding box
 ## and nuclei classified as G1 will have a red bounding box
 
